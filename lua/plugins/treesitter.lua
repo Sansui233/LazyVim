5cb51af7f65f2ba53c05011aa13b893c9ab21981
sansui233 <sansuilnm@gmail.com>
2025-08-01 06:43:15 +0800

    add disable to debug

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,17 @@
-return {
+return {{
   "nvim-treesitter/nvim-treesitter",
+  -- enabled = false,
   init = function()
     -- 插件加载前设置编译器
-    require("nvim-treesitter.install").compilers = { "zig" }
-  end,
-}
+    require("nvim-treesitter.install").compilers = {"zig"}
+  end
+}, {
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  -- enabled = false
+}, {
+  "nvim-ts-autotag",
+  -- enabled = false
+}, {
+  "folke/which-key.nvim",
+  -- enabled = false
+}}
